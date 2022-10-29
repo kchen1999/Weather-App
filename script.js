28e60d99d0e4477f29fd594564f7fcaabf4eb243
kchen1999 <kchen174175@gmail.com>
2022-10-29 22:34:39 +1100

    added functions to process JSOn data

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-function getTemp() {
-  console.log('');
+function getTemp(weatherData) {
+  return weatherData['main']['temp'];
 }
 
-function getFeelsLike() {
-  onsole.log('');
+function getFeelsLike(weatherData) {
+  return weatherData['main']['feels_like'];
 }
 
-function getState() {
-  onsole.log('');
+function getState(weatherData) {
+  return weatherData['weather'][0]['main'];
 }
 
-function getHumidity() {
-  console.log('');
+function getHumidity(weatherData) {
+  return weatherData['main']['humidity'];
 }
 
-function getSpeed() {
-  console.log('');
+function getWind(weatherData) {
+  return weatherData['wind']['speed'];
 
 }
 
 
 async function getWeatherData() {
   const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=Sydney&appid=e3cb8bb29dabde56f22e99d73ee8d1be',{mode: 'cors'})
-  const weatherData = response.json();
+  const weatherData= await response.json();
   console.log(weatherData);
-  //const temp = weatherData['main'];
-  //console.log(temp);
-  //const feelsLike = 
-  //const humidty = 
-  //const wind = 
-  //const state = 
+  const temp = console.log(getTemp(weatherData));
+  const feelsLike = console.log(getFeelsLike(weatherData));
+  const state = console.log(getState(weatherData));
+  const humidity = console.log(getHumidity(weatherData));
+  const wind = console.log(getWind(weatherData));
 }
 
 getWeatherData();
